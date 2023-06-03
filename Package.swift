0e9fd8cec2ed565cd7c4456e033efd1996662c1c
atacan <atacandur@icloud.com>
2023-06-03 23:50:50 +0200

    test pre-commit

--- Package.swift
+++ Package.swift
@@ -7,27 +7,28 @@ let package = Package(
     name: "DevBliss",
     platforms: [
         .macOS(.v12),
-        .iOS(.v15),
+        .iOS(.v15)
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(name: "Theme", targets: ["Theme"]),
         .library(name: "InputOutput", targets: ["InputOutput"]),
         .library(name: "ClipboardClient", targets: ["ClipboardClient"]),
-        .library(name: "HtmlToSwiftClient", targets: ["HtmlToSwiftClient"]),
+        .library(name: "HtmlToSwiftClient", targets: ["HtmlToSwiftClient"])
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         .package(url: "https://github.com/pointfreeco/swift-composable-architecture", from: "0.49.1"),
         .package(url: "https://github.com/pointfreeco/swift-dependencies", from: "0.1.0"),
-        .package(url: "https://github.com/stevengharris/SplitView", from: "3.1.0"),
+        .package(url: "https://github.com/stevengharris/SplitView", from: "3.1.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "Theme",
-            dependencies: []
+            dependencies: [
+            ]
         ),
         .target(
             name: "InputOutput",
@@ -35,7 +36,7 @@ let package = Package(
                 "ClipboardClient",
                 "Theme",
                 .product(name: "SplitView", package: "SplitView"),
-                .product(name: "ComposableArchitecture", package: "swift-composable-architecture"),
+                .product(name: "ComposableArchitecture", package: "swift-composable-architecture")
             ]
         ),
         .target(
@@ -51,6 +52,6 @@ let package = Package(
         .testTarget(
             name: "HtmlToSwiftClientTests",
             dependencies: ["HtmlToSwiftClient"]
-        ),
+        )
     ]
 )
