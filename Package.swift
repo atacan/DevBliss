0d15261fddb807b1b7e9ba86d56d8454098fe4d7
atacan <atacandur@icloud.com>
2023-06-03 23:39:21 +0200

    generic input output view

--- Package.swift
+++ Package.swift
@@ -6,22 +6,45 @@ import PackageDescription
 let package = Package(
     name: "DevBliss",
     platforms: [
-        .macOS(.v12)
+        .macOS(.v12),
+        .iOS(.v15),
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
-        .library(
-            name: "HtmlToSwiftClient",
-            targets: ["HtmlToSwiftClient"]
-        )
+        .library(name: "Theme", targets: ["Theme"]),
+        .library(name: "InputOutput", targets: ["InputOutput"]),
+        .library(name: "ClipboardClient", targets: ["ClipboardClient"]),
+        .library(name: "HtmlToSwiftClient", targets: ["HtmlToSwiftClient"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/pointfreeco/swift-composable-architecture", from: "0.49.1"),
+        .package(url: "https://github.com/pointfreeco/swift-dependencies", from: "0.1.0"),
+        .package(url: "https://github.com/stevengharris/SplitView", from: "3.1.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
+        .target(
+            name: "Theme",
+            dependencies: [
+            ]
+        ),
+        .target(
+            name: "InputOutput",
+            dependencies: [
+                "ClipboardClient",
+                "Theme",
+                .product(name: "SplitView", package: "SplitView"),
+                .product(name: "ComposableArchitecture", package: "swift-composable-architecture"),
+            ]
+        ),
+        .target(
+            name: "ClipboardClient",
+            dependencies: [
+                .product(name: "Dependencies", package: "swift-dependencies"),
+            ]
+        ),
         .target(
             name: "HtmlToSwiftClient",
             dependencies: []
