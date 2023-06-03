23e4161bc26eae3b36ba96d65b8ccc514142e2d9
atacan <atacandur@icloud.com>
2023-06-03 15:19:03 +0200

    format

--- Package.swift
+++ Package.swift
@@ -6,14 +6,14 @@ import PackageDescription
 let package = Package(
     name: "DevBliss",
     platforms: [
-        .macOS(.v12),
+        .macOS(.v12)
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "HtmlToSwiftClient",
             targets: ["HtmlToSwiftClient"]
-        ),
+        )
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
