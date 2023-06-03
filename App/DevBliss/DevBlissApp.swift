a23ddf6018e0016a81619342d67dc18ca21abaf0
atacan <atacandur@icloud.com>
2023-06-03 15:02:57 +0200

    initial prep

---
+++ App/DevBliss/DevBlissApp.swift
@@ -0,0 +1,14 @@
+//
+// https://github.com/atacan
+// 03.06.23
+
+import SwiftUI
+
+@main
+struct DevBlissApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
