53d4da08378d5402c3e57055c8a2d75e838e0325
atacan <atacandur@icloud.com>
2023-06-04 12:22:47 +0200

    library and component selection

--- Sources/HtmlToSwiftClient/HtmlToSwiftClient.swift
+++ Sources/HtmlToSwiftClient/HtmlToSwiftClient.swift
@@ -4,6 +4,10 @@ import HtmlSwift
 public struct HtmlToSwiftClient {
     public var binaryBirds: @Sendable (String) async throws -> String
     public var pointfreeco: @Sendable (String) async throws -> String
+
+    public func convert(_ html: String, for dsl: SwiftDSL, output: HtmlOutputComponent) async throws -> String {
+        try htmlToSwift(html, for: dsl, component: output)
+    }
 }
 
 extension HtmlToSwiftClient: DependencyKey {
