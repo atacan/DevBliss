40868451b4a2894ebf331453693cdd5f7aea95f7
atacan <atacandur@icloud.com>
2023-06-17 16:54:02 +0200

    refactor handleOtherTool and new regex tool

--- Sources/AppFeature/AppReducer.swift
+++ Sources/AppFeature/AppReducer.swift
@@ -1,11 +1,12 @@
 import ComposableArchitecture
 import HtmlToSwiftFeature
 import JsonPrettyFeature
+import PrefixSuffixFeature
+import RegexMatchesFeature
 import SharedModels
 import SwiftUI
 import TextCaseConverterFeature
 import UUIDGeneratorFeature
-import PrefixSuffixFeature
 
 public struct AppReducer: ReducerProtocol {
     public init() {}
@@ -15,19 +16,22 @@ public struct AppReducer: ReducerProtocol {
         @PresentationState var textCaseConverter: TextCaseConverterReducer.State?
         @PresentationState var uuidGenerator: UUIDGeneratorReducer.State?
         @PresentationState var prefixSuffix: PrefixSuffixReducer.State?
+        @PresentationState var regexMatches: RegexMatchesReducer.State?
 
         public init(
             htmlToSwift: HtmlToSwiftReducer.State? = nil,
             jsonPretty: JsonPrettyReducer.State? = nil,
             textCaseConverter: TextCaseConverterReducer.State? = nil,
             uuidGenerator: UUIDGeneratorReducer.State? = nil,
-            prefixSuffix: PrefixSuffixReducer.State? = nil
+            prefixSuffix: PrefixSuffixReducer.State? = nil,
+            regexMatches: RegexMatchesReducer.State? = nil
         ) {
             self.htmlToSwift = htmlToSwift
             self.jsonPretty = jsonPretty
             self.textCaseConverter = textCaseConverter
             self.uuidGenerator = uuidGenerator
             self.prefixSuffix = prefixSuffix
+            self.regexMatches = regexMatches
         }
     }
 
@@ -37,6 +41,7 @@ public struct AppReducer: ReducerProtocol {
         case textCaseConverter(PresentationAction<TextCaseConverterReducer.Action>)
         case uuidGenerator(PresentationAction<UUIDGeneratorReducer.Action>)
         case prefixSuffix(PresentationAction<PrefixSuffixReducer.Action>)
+        case regexMatches(PresentationAction<RegexMatchesReducer.Action>)
         case navigationLinkTouched(Tool)
     }
 
@@ -46,25 +51,37 @@ public struct AppReducer: ReducerProtocol {
             case let .htmlToSwift(
                 .presented(.inputOutput(.output(.outputControls(.inputOtherToolButtonTouched(otherTool)))))
             ):
-                handleOtherTool(thisTool: .htmlToSwift, otherTool: otherTool, state: &state)
+                handleOtherTool(thisToolOutput: state.htmlToSwift?.outputText, otherTool: otherTool, state: &state)
                 return .none
             case let .jsonPretty(
                 .presented(.inputOutput(.output(.outputControls(.inputOtherToolButtonTouched(otherTool)))))
             ):
-                handleOtherTool(thisTool: .jsonPretty, otherTool: otherTool, state: &state)
+                handleOtherTool(thisToolOutput: state.jsonPretty?.outputText, otherTool: otherTool, state: &state)
                 return .none
             case let .textCaseConverter(
                 .presented(.inputOutput(.output(.outputControls(.inputOtherToolButtonTouched(otherTool)))))
             ):
-                handleOtherTool(thisTool: .textCaseConverter, otherTool: otherTool, state: &state)
+                handleOtherTool(thisToolOutput: state.textCaseConverter?.outputText, otherTool: otherTool, state: &state)
                 return .none
             case let .uuidGenerator(
                 .presented(.output(.outputControls(.inputOtherToolButtonTouched(otherTool))))
             ):
-                handleOtherTool(thisTool: .uuidGenerator, otherTool: otherTool, state: &state)
+                handleOtherTool(thisToolOutput: state.uuidGenerator?.outputText, otherTool: otherTool, state: &state)
                 return .none
-            case let .prefixSuffix(.presented(.inputOutput(.output(.outputControls(.inputOtherToolButtonTouched(otherTool)))))):
-                handleOtherTool(thisTool: .prefixSuffix, otherTool: otherTool, state: &state)
+            case let .prefixSuffix(
+                .presented(.inputOutput(.output(.outputControls(.inputOtherToolButtonTouched(otherTool)))))
+            ):
+                handleOtherTool(thisToolOutput: state.prefixSuffix?.outputText, otherTool: otherTool, state: &state)
+                return .none
+            case let .regexMatches(
+                .presented(.inputOutput(.output(.outputControls(.inputOtherToolButtonTouched(otherTool)))))
+            ):
+                handleOtherTool(thisToolOutput: state.regexMatches?.outputText, otherTool: otherTool, state: &state)
+                return .none
+            case let .regexMatches(
+                .presented(.inputOutput(.outputSecond(.outputControls(.inputOtherToolButtonTouched(otherTool)))))
+            ):
+                handleOtherTool(thisToolOutput: state.regexMatches?.outputSecondText, otherTool: otherTool, state: &state)
                 return .none
             case .htmlToSwift:
                 return .none
@@ -75,6 +92,8 @@ public struct AppReducer: ReducerProtocol {
             case .uuidGenerator:
                 return .none
             case .prefixSuffix:
+                return .none
+            case .regexMatches:
                 return .none
 
             case let .navigationLinkTouched(tool):
@@ -94,6 +113,9 @@ public struct AppReducer: ReducerProtocol {
                 case .prefixSuffix:
                     state.prefixSuffix = .init()
                     return .none
+                case .regexMatches:
+                    state.regexMatches = .init()
+                    return .none
                 }
             }
         }
@@ -112,51 +134,27 @@ public struct AppReducer: ReducerProtocol {
         .ifLet(\.$prefixSuffix, action: /Action.prefixSuffix) {
             PrefixSuffixReducer()
         }
+        .ifLet(\.$regexMatches, action: /Action.regexMatches) {
+            RegexMatchesReducer()
+        }
     }
 
-    private func handleOtherTool(thisTool: Tool, otherTool: Tool, state: inout State) {
-        switch (thisTool, otherTool) {
-        //
-        case (.htmlToSwift, .jsonPretty):
-            state.jsonPretty = JsonPrettyReducer.State(input: state.htmlToSwift?.outputText ?? "")
-        case (.htmlToSwift, .textCaseConverter):
-            state.textCaseConverter = TextCaseConverterReducer.State(input: state.htmlToSwift?.outputText ?? "")
-        case (.htmlToSwift, .prefixSuffix):
-            state.prefixSuffix = PrefixSuffixReducer.State(input: state.htmlToSwift?.outputText ?? "")
-        //
-        case (.jsonPretty, .htmlToSwift):
-            state.htmlToSwift = HtmlToSwiftReducer.State(input: state.jsonPretty?.outputText ?? "")
-        case (.jsonPretty, .textCaseConverter):
-            state.textCaseConverter = TextCaseConverterReducer.State(input: state.jsonPretty?.outputText ?? "")
-        case (.jsonPretty, .prefixSuffix):
-            state.prefixSuffix = PrefixSuffixReducer.State(input: state.jsonPretty?.outputText ?? "")
-        //
-        case (.textCaseConverter, .htmlToSwift):
-            state.htmlToSwift = HtmlToSwiftReducer.State(input: state.textCaseConverter?.outputText ?? "")
-        case (.textCaseConverter, .jsonPretty):
-            state.jsonPretty = JsonPrettyReducer.State(input: state.textCaseConverter?.outputText ?? "")
-        case (.textCaseConverter, .prefixSuffix):
-            state.prefixSuffix = PrefixSuffixReducer.State(input: state.textCaseConverter?.outputText ?? "")
-        //
-        case (.uuidGenerator, .htmlToSwift):
-            state.htmlToSwift = HtmlToSwiftReducer.State(input: state.uuidGenerator?.outputText ?? "")
-        case (.uuidGenerator, .jsonPretty):
-            state.jsonPretty = JsonPrettyReducer.State(input: state.uuidGenerator?.outputText ?? "")
-        case (.uuidGenerator, .textCaseConverter):
-            state.textCaseConverter = TextCaseConverterReducer.State(input: state.uuidGenerator?.outputText ?? "")
-        case (.uuidGenerator, .prefixSuffix):
-            state.prefixSuffix = PrefixSuffixReducer.State(input: state.uuidGenerator?.outputText ?? "")
-        //
-        case (.prefixSuffix, .htmlToSwift):
-            state.htmlToSwift = HtmlToSwiftReducer.State(input: state.prefixSuffix?.outputText ?? "")
-        case (.prefixSuffix, .jsonPretty):
-            state.jsonPretty = JsonPrettyReducer.State(input: state.prefixSuffix?.outputText ?? "")
-        case (.prefixSuffix, .textCaseConverter):
-            state.textCaseConverter = TextCaseConverterReducer.State(input: state.prefixSuffix?.outputText ?? "")
-        
-        default:
-            return
+    private func handleOtherTool(thisToolOutput: String?, otherTool: Tool, state: inout State) {
+        switch otherTool {
+        case .htmlToSwift:
+            state.htmlToSwift = HtmlToSwiftReducer.State(input: thisToolOutput ?? "")
+        case .jsonPretty:
+            state.jsonPretty = JsonPrettyReducer.State(input: thisToolOutput ?? "")
+        case .textCaseConverter:
+            state.textCaseConverter = TextCaseConverterReducer.State(input: thisToolOutput ?? "")
+        case .prefixSuffix:
+            state.prefixSuffix = PrefixSuffixReducer.State(input: thisToolOutput ?? "")
+        case .regexMatches:
+            state.regexMatches = RegexMatchesReducer.State(input: thisToolOutput ?? "")
+        case .uuidGenerator:
+            break
         }
+
     }
 }
 
@@ -178,8 +176,8 @@ public struct AppView: View {
                         viewStore.send(.navigationLinkTouched(.htmlToSwift))
                     } destination: { store in
                         HtmlToSwiftView(store: store)
-                            .navigationTitle("Convert Html code to a DSL in Swift")
-                            .padding(.top)
+                        .navigationTitle("Convert Html code to a DSL in Swift")
+                        .padding(.top)
                     } label: {
                         Text("Html to Swift")
                     }
@@ -190,8 +188,8 @@ public struct AppView: View {
                         viewStore.send(.navigationLinkTouched(.textCaseConverter))
                     } destination: { store in
                         TextCaseConverterView(store: store)
-                            .navigationTitle("Convert case of list of words")
-                            .padding(.top)
+                        .navigationTitle("Convert case of list of words")
+                        .padding(.top)
                     } label: {
                         Text("Text Case")
                     }
@@ -202,10 +200,22 @@ public struct AppView: View {
                         viewStore.send(.navigationLinkTouched(.prefixSuffix))
                     } destination: { store in
                         PrefixSuffixView(store: store)
-                            .navigationTitle("Replace and add prefix or suffix to each line")
-                            .padding(.top)
+                        .navigationTitle("Replace and add prefix or suffix to each line")
+                        .padding(.top)
                     } label: {
                         Text("Prefix Suffix")
+                    }
+
+                    NavigationLinkStore(
+                        store.scope(state: \.$regexMatches, action: { .regexMatches($0) })
+                    ) {
+                        viewStore.send(.navigationLinkTouched(.regexMatches))
+                    } destination: { store in
+                        RegexMatchesView(store: store)
+                        .navigationTitle("Regex Matches")
+                        .padding(.top)
+                    } label: {
+                        Text("Regex Matches")
                     }
                 }
 
@@ -216,8 +226,8 @@ public struct AppView: View {
                         viewStore.send(.navigationLinkTouched(.jsonPretty))
                     } destination: { store in
                         JsonPrettyView(store: store)
-                            .navigationTitle("Pretty print and Highlight Json")
-                            .padding(.top)
+                        .navigationTitle("Pretty print and Highlight Json")
+                        .padding(.top)
                     } label: {
                         Text("Json")
                     }
@@ -230,22 +240,23 @@ public struct AppView: View {
                         viewStore.send(.navigationLinkTouched(.uuidGenerator))
                     } destination: { store in
                         UUIDGeneratorView(store: store)
-                            .navigationTitle("Generate UUIDs")
-                            .padding(.top)
+                        .navigationTitle("Generate UUIDs")
+                        .padding(.top)
                     } label: {
                         Text("UUID")
                     }
                 }
             }
             .listStyle(.sidebar)
-            .frame(minWidth: 150) // to keep the toggle-sidebar button above the sidebar
+            .frame(minWidth: 150)  // to keep the toggle-sidebar button above the sidebar
             #if os(macOS)
                 // it falls behind window toolbar and becomes unclickable
                 // .padding(.top)
                 .toolbar {
                     ToolbarItem {
                         Button {
-                            NSApp.keyWindow?.firstResponder?.tryToPerform(
+                            NSApp.keyWindow?.firstResponder?
+                            .tryToPerform(
                                 #selector(NSSplitViewController.toggleSidebar(_:)),
                                 with: nil
                             )
