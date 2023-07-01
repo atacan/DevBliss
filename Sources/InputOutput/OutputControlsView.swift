f0efd8b9c09d84cf10e1833cfcf26982481aadd6
atacan <atacandur@icloud.com>
2023-07-01 19:45:37 +0200

    dismiss button for the popover ios

--- Sources/InputOutput/OutputControlsView.swift
+++ Sources/InputOutput/OutputControlsView.swift
@@ -69,23 +69,23 @@ struct OutputControlsView: View {
             }  // <-Button
             .foregroundColor(
                 viewStore.copyButtonAnimating
-                    ? ThemeColor.Text.success
-                    : ThemeColor.Text.controlText
+                ? ThemeColor.Text.success
+                : ThemeColor.Text.controlText
             )
             .font(.footnote)
             .keyboardShortcut("c", modifiers: [.command, .shift])
             .help("Copy to clipboard (Command+Shift+C)")
-
+            
             Button {
                 viewStore.send(.saveAsButtonTouched)
             } label: {
                 Image(systemName: "opticaldiscdrive")
             }  // <-Button
-
+            
             .font(.footnote)
             .keyboardShortcut("s", modifiers: [.command, .shift])
             .help("Save to disk (Command+Shift+S)")
-
+            
             Button {
                 isOtherToolsPopoverVisible = true
             } label: {
@@ -95,30 +95,56 @@ struct OutputControlsView: View {
             .keyboardShortcut("u", modifiers: [.command, .shift])
             .help("Input it to the other tools (Command+Shift+U)")
             .popover(isPresented: $isOtherToolsPopoverVisible) {
+                
                 VStack(alignment: .leading) {
-                    HStack(alignment: .lastTextBaseline) {
-                        Image(systemName: "square.and.pencil")
-                        Text("Move the output to one of the tools as input")
-                            .lineLimit(nil)
-                            .font(.headline)
-                    }
-                        .padding(.bottom)
-                    ForEach(Tool.allCases.filter(\.isInputtable)) { tool in
+#if os(macOS)
+                    popContent
+#elseif os(iOS)
+                    HStack(alignment: .center) {
+                        Spacer()
                         Button(action: {
                             isOtherToolsPopoverVisible = false
-                            viewStore.send(.inputOtherToolButtonTouched(tool))
                         }) {
-                            Text(tool.name)
-                                .frame(maxWidth: .infinity, alignment: .leading)
-                                .background(Color.gray.opacity(0.01))
+                            Image(systemName: "xmark.circle")
+                                .opacity(0.8)
                         }
+                        .padding()
                         .buttonStyle(.plain)
-                        Divider()
-                    }  // <-ForEach
+                    } // <-HStack
+                    
+                    Spacer()
+                    popContent
+                    Spacer()
+#endif
                 }  // <-VStack
                 .padding()
+                
             }
         }
+    }
+    
+    var popContent: some View  {
+        Group {
+        HStack(alignment: .lastTextBaseline) {
+                    Image(systemName: "square.and.pencil")
+                    Text("Move the output to one of the tools as input")
+                        .lineLimit(nil)
+                        .font(.headline)
+                }
+                    .padding(.bottom)
+                ForEach(Tool.allCases.filter(\.isInputtable)) { tool in
+                    Button(action: {
+                        isOtherToolsPopoverVisible = false
+                        viewStore.send(.inputOtherToolButtonTouched(tool))
+                    }) {
+                        Text(tool.name)
+                            .frame(maxWidth: .infinity, alignment: .leading)
+                            .background(Color.gray.opacity(0.01))
+                    }
+                    .buttonStyle(.plain)
+                    Divider()
+                }  // <-ForEach
+        } // <-Group
     }
 }
 
