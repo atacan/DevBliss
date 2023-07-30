a758aaf57d93df13bb7cea76f41040ac794e2425
atacan <atacandur@icloud.com>
2023-07-30 13:04:55 +0200

    delete all the preview apps

--- App/PreviewSwiftPretty/ContentView.swift
+++
@@ -1,9 +0,0 @@
-import ComposableArchitecture
-import SwiftPrettyFeature
-import SwiftUI
-
-struct ContentView: View {
-    var body: some View {
-        SwiftPrettyView(store: Store(initialState: .init(), reducer: SwiftPrettyReducer()))
-    }
-}
